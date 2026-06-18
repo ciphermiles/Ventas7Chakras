51a4f95c4ab01c3480e76a866dd12f9560626d5c
ciphermiles <ciphermiles16@gmail.com>
2026-06-17 21:26:55 -0600

    Update cloud-config.js

--- cloud-config.js
+++ cloud-config.js
@@ -1,7 +1,9 @@
 window.PUNTONEXO_CLOUD = {
   enabled: true,
   provider: "supabase",
+  authMode: "supabase",
+  authEmailDomain: "ventas7chakras.local",
   supabaseUrl: "https://gfzibyaurqfbwejtqhsk.supabase.co",
-  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdmemlieWF1cnFmYndlanRxaHNrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA1MjI2MjcsImV4cCI6MjA5NjA5ODYyN30.J3IfV3EL4-WiYJ8VcwoTnX-pNd0HRYKDg8_4z0g2S0w",
+  supabaseAnonKey: "sb_publishable_aEPMS0F4fChPKTyC-l_X3g_OHKir29K",
   businessId: "tienda-principal"
 };
